76f609f04833a50e4d03a02128c2de97f79cf3d2
hurkadlisagar06-art <hurkadlisagar06@gmail.com>
2025-10-06 02:04:28 +0530

    save

---
+++ studentgrade.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter your marks out of 100: "))
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+elif marks >= 60:
+    print("Grade: D")
+elif marks >= 50:
+    print("Grade: E")
+else :
+    print("Grade: F")
+
